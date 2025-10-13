7bbcd925d725b427980a13702a6c4718ee5d1f6d
Ubuntu <ubuntu@ip-10-0-3-50.ap-southeast-1.compute.internal>
2025-10-13 21:26:39 +0800

    Testing with best

---
+++ tools/run_best_minimap_pair_end.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+read_file_1=$1
+read_file_2=$2
+reference_file=$3
+output_prefix=$5
+output_dir=$4
+
+minimap2_path="./tools/minimap2/minimap2"
+best_path="./tools/best"
+
+mkdir -p ${output_dir}
+
+# Run minimap2
+${minimap2_path} -ax sr ${reference_file} ${read_file_1} ${read_file_2} | samtools sort -o ${output_dir}/${output_prefix}.bam --write-index - 
+
+# Run best
+${best_path} ${output_dir}/${output_prefix}.bam ${reference_file} ${output_dir}/${output_prefix}
