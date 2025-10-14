bb4789320faeeb3bac32131a44bec9bb63cd7c13
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2025-10-14 20:39:03 +0800

    local commit

--- experiments/simulated_data/run_experiments.sh
+++ experiments/simulated_data/run_experiments.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_depth_1_id_96.fasta.gz ./data/reference/Ecoli_K12_MG1655.fasta ./output/simulated_data Ecoli_test
+./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_random_depth_1_id_96.fasta.gz ./data/reference/Ecoli_K12_MG1655.fasta ./output/simulated_data Ecoli_test
 ./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_depth_1_id_96.fasta.gz ./data/reference/Ecoli_O157_H7.fasta ./output/simulated_data Ecoli_test
