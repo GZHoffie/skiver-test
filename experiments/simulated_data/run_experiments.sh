6c01b19fceb287fa5b1ba06ff88398c3bf8c4944
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2025-10-15 16:47:19 +0800

    Some more tests

--- experiments/simulated_data/run_experiments.sh
+++ experiments/simulated_data/run_experiments.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_random_depth_1_id_96.fasta.gz ./data/reference/Ecoli_K12_MG1655.fasta ./output/simulated_data Ecoli_test
+~/kv-mer/target/release/kvmer analyze ./data/simulated_data/Ecoli_K12_MG1655_random_depth_1_id_96.fasta.gz > ./output/ecoli_test.tsv
+
+./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_random_depth_32_id_96.fasta.gz ./data/reference/Ecoli_K12_MG1655.fasta ./output/simulated_data Ecoli_test
 ./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_depth_1_id_96.fasta.gz ./data/reference/Ecoli_O157_H7.fasta ./output/simulated_data Ecoli_test
