491240fc8db41dd6703512a80e7399372f5e7e68
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2025-10-16 15:20:41 +0800

    Slightly adjust script

--- experiments/simulated_data/generate_data.sh
+++ experiments/simulated_data/generate_data.sh
@@ -2,7 +2,7 @@ identity=(100 98 96 94 92 90)
 coverage=(32)
 output_path="./data/simulated_data"
 genome_path="./data/reference/Ecoli_K12_MG1655.fasta"
-identifier="Ecoli_K12_MG1655_random"
+identifier="Ecoli_K12_MG1655_random_uniform"
 
 
 mkdir -p $output_path
@@ -12,7 +12,7 @@ for depth in ${coverage[@]}; do
     output_file=${output_path}/${identifier}_depth_${depth}_id_${id}.fasta.gz
     badread simulate --reference ${genome_path} --quantity ${depth}x --error_model "random" \
         --qscore_model ideal --glitches 0,0,0 --junk_reads 0 --random_reads 0 --chimeras 0 \
-        --identity ${id},100,2.5 --length 2000,1000 --start_adapter_seq "" --end_adapter_seq "" \
+        --identity ${id},100,0 --length 2000,1000 --start_adapter_seq "" --end_adapter_seq "" \
         | gzip > ${output_file}
   done
 done
--- experiments/simulated_data/run_experiments.sh
+++ experiments/simulated_data/run_experiments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-~/kv-mer/target/release/kvmer analyze ./data/simulated_data/Ecoli_K12_MG1655_random_depth_1_id_96.fasta.gz > ./output/ecoli_test.tsv
+~/kv-mer/target/release/kvmer analyze ./data/simulated_data/Ecoli_K12_MG1655_random_depth_32_id_96.fasta.gz -k 21 -v 6 -c 200 -t 5 > ./output/ecoli_test.tsv
 
 ./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_random_depth_32_id_96.fasta.gz ./data/reference/Ecoli_K12_MG1655.fasta ./output/simulated_data Ecoli_test
 ./tools/run_best_minimap.sh ./data/simulated_data/Ecoli_K12_MG1655_depth_1_id_96.fasta.gz ./data/reference/Ecoli_O157_H7.fasta ./output/simulated_data Ecoli_test
