ed31e8b139f066486eac74938704f0850d93aa51
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2025-11-25 16:41:17 +0800

    Quality trimming by cutting

--- experiments/quality_trimming/run_quality_trimming.sh
+++ experiments/quality_trimming/run_quality_trimming.sh
@@ -6,10 +6,17 @@ mkdir -p $output_dir
 fastp \
   -i ./data/zymo/ERR2935851_1.fastq.gz -I ./data/zymo/ERR2935851_2.fastq.gz \
   -o ./data/zymo/ERR2935851_1.trim.fastq.gz -O ./data/zymo/ERR2935851_2.trim.fastq.gz \
-  --detect_adapter_for_pe \
   --cut_tail \
   --cut_window_size 4 \
-  --cut_mean_quality 20 \
   --length_required 50 \
   --thread 8 \
   --html $output_dir/ERR2935851.html --json $output_dir/ERR2935851.json
+
+
+/usr/bin/time -o $output_dir/ERR3152366.time -v \
+fastplong \
+  -i ./data/zymo/ERR3152366.fastq.gz \
+  -o ./data/zymo/ERR3152366.trim.fastq.gz \
+  -f 100 \
+  -t 100 \
+  --html $output_dir/ERR3152366.html --json $output_dir/ERR3152366.json
