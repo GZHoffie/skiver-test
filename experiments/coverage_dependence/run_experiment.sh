9c3b498c0687a8ce1443bb086e3bfb639767369f
Gu Zhenhao <zhenhao.gu@u.nus.edu>
2026-01-06 01:13:23 +0800

    Simulated results

--- experiments/coverage_dependence/run_experiment.sh
+++ experiments/coverage_dependence/run_experiment.sh
@@ -1,16 +1,16 @@
-output_dir="./output/coverage_dependence"
+output_dir="./output/coverage_dependence_homogeneous_consensus_l2"
 
 mkdir -p $output_dir
 mkdir -p ${output_dir}/log
 
 kvmer_dir="../kv-mer/target/release/kvmer"
-simulated_data_prefix="./data/simulated_data/Ecoli_K12_MG1655_random_depth_64"
+simulated_data_prefix="./data/simulated_data/Ecoli_K12_MG1655_random_depth_128"
 subsample_script="./experiments/coverage_dependence/subsample_reads.sh"
-read_identity=(98) #(90 92 94 96 98)
+read_identity=(90 92 94 96 98 100)
 # 64x, 32x, 16x, 8x, 4x, 2x, 1x
-original_coverage=64
-subsample_coverage=(8 4 2 1)
-num_experiments=2 #20
+original_coverage=128
+subsample_coverage=(64 32 16 8 4 2 1)
+num_experiments=20
 
 chmod +x $subsample_script
 
@@ -21,8 +21,10 @@ for id in ${read_identity[@]}; do
     for exp_num in $(seq 1 $num_experiments); do
       output_prefix="Ecoli_K12_MG1655_depth_${cov}_id_${id}_exp_${exp_num}"
       $subsample_script ${input_file} ${subsample_rate} ./temp.fastq
-      /usr/bin/time -o ${output_dir}/log/${output_prefix}.time -v ${kvmer_dir} analyze ./temp.fastq -l 2 > ${output_dir}/${output_prefix}.csv
-      /usr/bin/time -o ${output_dir}/log/${output_prefix}_bidirectional.time -v ${kvmer_dir} analyze ./temp.fastq -l 2 --bidirectional > ${output_dir}/${output_prefix}_bidirectional.csv
+      #/usr/bin/time -o ${output_dir}/log/${output_prefix}.time -v ${kvmer_dir} analyze ./temp.fastq > ${output_dir}/${output_prefix}.csv
+      #/usr/bin/time -o ${output_dir}/log/${output_prefix}_bidirectional.time -v ${kvmer_dir} analyze ./temp.fastq --bidirectional > ${output_dir}/${output_prefix}_bidirectional.csv
+      ${kvmer_dir} analyze ./temp.fastq -l 2 > ${output_dir}/${output_prefix}.csv
+      ${kvmer_dir} analyze ./temp.fastq --bidirectional -l 2 > ${output_dir}/${output_prefix}_bidirectional.csv
       rm ./temp.fastq
     done
   done
